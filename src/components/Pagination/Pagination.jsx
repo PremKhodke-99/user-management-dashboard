91ac8c068977ad8f3f5dc0dfebf4abb8c0f95fb9
Prem <premkhodke.99@gmail.com>
2024-05-16 23:42:17 +0530

    Pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,15 +1,52 @@
 import React from 'react'
 import { MdKeyboardArrowRight, MdKeyboardDoubleArrowRight, MdKeyboardArrowLeft, MdKeyboardDoubleArrowLeft } from "react-icons/md";
-import '../index.css';
+import styles from './Pagination.module.css'
 
-const Pagination = (props) => {
+const Pagination = ({ nPages, currentPage, setCurrentPage }) => {
+
+  const pageNumbers = [...Array(nPages + 1).keys()].slice(1);
+
+  const nextPage = () => {
+    if (currentPage !== nPages) setCurrentPage(prev => prev + 1);
+  }
+
+  const prevPage = () => {
+    if (currentPage !== 1) setCurrentPage(prev => prev - 1);
+  }
+
   return (
-    <div className='paginationwrapper'>
-      <button><MdKeyboardDoubleArrowLeft /></button>
-      <button><MdKeyboardArrowLeft /></button>
-      <button></button>
-      <button><MdKeyboardArrowRight /></button>
-      <button><MdKeyboardDoubleArrowRight /></button>
+    <div className={styles.paginationwrapper}>
+      <button className={currentPage == 1 ? styles.inactive : styles.button}
+        onClick={() => setCurrentPage(1)}
+      >
+        <MdKeyboardDoubleArrowLeft />
+      </button>
+      <button className={currentPage == 1 ? styles.inactive : styles.button}
+        onClick={prevPage}
+      >
+        <MdKeyboardArrowLeft />
+      </button>
+      {
+        pageNumbers.map((pgno) => (
+          <button
+            key={pgno}
+            className={currentPage == pgno ? styles.active : styles.button}
+            onClick={() => setCurrentPage(pgno)}
+          >
+            {pgno}
+          </button>
+        ))
+      }
+      <button className={currentPage == nPages ? styles.inactive : styles.button}
+        onClick={nextPage}
+      >
+        <MdKeyboardArrowRight />
+      </button>
+      <button className={currentPage == nPages ? styles.inactive : styles.button}
+        onClick={() => setCurrentPage(nPages)}
+      >
+        <MdKeyboardDoubleArrowRight />
+      </button>
     </div>
   )
 }
