8068e2188e4e86c00208544a5a8c929b589c93ed
Prem <premkhodke.99@gmail.com>
2024-05-17 11:36:28 +0530

    Navbar edit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import styles from './Navbar.module.css';
 
-const Navbar = () => {
+const Navbar = ({ formActive, setFormActive }) => {
+
   return (
     <nav className={styles.navbar}>
-        <h1>User Management Dashboard</h1>
+      <h1>User Management Dashboard</h1>
+      <button className={styles.addNew}
+        onClick={() => setFormActive(prev => !prev)}
+      >
+        {
+          formActive ? "Close" : "Add user"
+        }
+      </button>
     </nav>
   )
 }
