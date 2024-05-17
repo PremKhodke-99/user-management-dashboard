e966e61b6bf11b0dc3e77782eea05e94d597ad71
Prem <premkhodke.99@gmail.com>
2024-05-17 11:35:45 +0530

    router removal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import Navbar from "./components/Navbar/Navbar"
 import Table from "./components/Table/Table";
+import Form from "./components/Form/Form";
 import axios from "axios";
 import Pagination from "./components/Pagination/Pagination";
 
@@ -8,9 +9,10 @@ function App() {
 
   const [data, setData] = useState([]);
   const [error, setError] = useState("");
+  const [formActive, setFormActive] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const dataPerPage = 3;
+  const dataPerPage = 4;
   const indexOfLastData = currentPage * dataPerPage;
   const indexOfFirstData = indexOfLastData - dataPerPage;
   const currentData = data.slice(indexOfFirstData, indexOfLastData);
@@ -20,27 +22,37 @@ function App() {
     axios.get('https://jsonplaceholder.typicode.com/users')
       .then((res) => setData(res.data))
       .catch((err) => setError(err.message));
-
-    
   }, []);
 
   console.log(data)
 
   return (
     <div>
-      <Navbar />
-      <Table
-        currentData={currentData}
-        // nPages={nPages}
-        // currentPage={currentPage}
-        // setCurrentPage={setCurrentPage}
-        error={error}
-      />
-      <Pagination
-        nPages={nPages}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
+      <Navbar formActive={formActive} setFormActive={setFormActive} />
+
+      {
+        !formActive ? (
+          <>
+            <Table
+              currentData={currentData}
+              error={error}
+            />
+            <Pagination
+              nPages={nPages}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+            />
+          </>
+        ) : (
+          <Form
+            data={data}
+            setData={setData}
+            setFormActive={setFormActive}
+          />
+        )
+      }
+
+
     </div>
   )
 }
